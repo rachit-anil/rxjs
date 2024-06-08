5abbe1c2dc681bb47bc0e0df60a6d6c7e348045d
rachit-anil <rachit9910102312@gmail.com>
2024-06-08 21:09:39 +0530

    custom observable creation

--- master-rxjs-6-without-breaking-a-sweat-master/206-from/index.js
+++ master-rxjs-6-without-breaking-a-sweat-master/206-from/index.js
@@ -1,52 +1,24 @@
-import { from } from 'rxjs';
+import { from } from "rxjs";
 
-const btnStart = document.getElementById('btnStart');
-const result = document.getElementById('result');
-const liElements = document.querySelectorAll('li');
+const btnStart = document.getElementById("btnStart");
+const result = document.getElementById("result");
+const liElements = document.querySelectorAll("li");
 
-btnStart.addEventListener('click', () => {
+btnStart.addEventListener("click", () => {
   // from([1, 2, 3]).subscribe(e => console.log(e));
 
-  from(liElements).subscribe(el => {
-    el.textContent = 'using from()';
+  console.log("start");
+
+  from(liElements).subscribe((el) => {
+    console.log(el);
+    el.textContent = "using from()";
   });
 
-  const p = new Promise(resolve => setTimeout(() => resolve(42), 2000));
-  from(p).subscribe(e => (result.textContent = e));
+  const p = new Promise((resolve) => setTimeout(() => resolve(42), 2000));
+
+  from(p).subscribe((e) => {
+    result.textContent = e;
+    console.log(e);
+  });
+  console.log("end");
 });
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-if (module.hot) {
-  module.hot.dispose(function() {
-    location.reload();
-  });
-}
--- master-rxjs-6-without-breaking-a-sweat-master/208-custom/index.js
+++ master-rxjs-6-without-breaking-a-sweat-master/208-custom/index.js
@@ -1,62 +1,50 @@
-import { interval, Observable, map } from 'rxjs';
+import { interval, Observable, map, switchMap, of, mergeMap } from "rxjs";
 
-const btnStart = document.getElementById('btnStart');
-const result = document.getElementById('result');
+const btnStart = document.getElementById("btnStart");
+const result = document.getElementById("result");
 
+//Solution 3:
 function time(ms) {
-  // return interval(ms).pipe(map(() => new Date().toLocaleTimeString()));
-
-  return new Observable(subscriber => {
+  // Create an observable using Observable.create (or new Observable)
+  return new Observable((subscriber) => {
     // subscriber.next('Hello')
     const handle = setInterval(() => {
       console.log(new Date().toLocaleTimeString());
       subscriber.next(new Date().toLocaleTimeString());
     }, ms);
 
+    // Cleanup logic when the observable is unsubscribed
     return () => clearInterval(handle);
   });
 }
 
-btnStart.addEventListener('click', () => {
-  const subscription = time(1000).subscribe(time => {
+btnStart.addEventListener("click", () => {
+  const subscription = time(1000).subscribe((time) => {
     result.textContent = time;
   });
 
   setTimeout(() => subscription.unsubscribe(), 5000);
 });
 
-/*
+// Solution 2
+// function time(ms) {
+//   return interval(ms).pipe(map(() => new Date().toLocaleTimeString()));
+// }
 
+// time(1000).subscribe((res) => {
+//   console.log(res);
+//   result.textContent = res;
+// });
 
+// Solution 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-if (module.hot) {
-  module.hot.dispose(function() {
-    location.reload();
-  });
-}
+// interval(1000)
+//   .pipe(
+//     switchMap(() => {
+//       return of(new Date().toLocaleTimeString());
+//     })
+//   )
+//   .subscribe((res) => {
+//     console.log(res);
+//     result.textContent = res;
+//   });
