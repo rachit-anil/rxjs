8b5864003008a5db42ed1c8eaff73ff9df5d5395
rachit-anil <rachit9910102312@gmail.com>
2024-06-09 13:09:11 +0530

    Error handling

--- master-rxjs-6-without-breaking-a-sweat-master/401-prevent-error-completing/index.js
+++ master-rxjs-6-without-breaking-a-sweat-master/401-prevent-error-completing/index.js
@@ -1,58 +1,57 @@
-import { fromEvent, throwError, of, mergeMap, tap, catchError } from 'rxjs';
+import {
+  fromEvent,
+  throwError,
+  of,
+  mergeMap,
+  tap,
+  catchError,
+  map,
+} from "rxjs";
 
-const btnStart = document.getElementById('btnStart');
-const btnClear = document.getElementById('btnClear');
-const result = document.getElementById('result');
+const btnStart = document.getElementById("btnStart");
+const btnClear = document.getElementById("btnClear");
+const result = document.getElementById("result");
 
-fromEvent(btnStart, 'click')
+fromEvent(btnStart, "click")
   .pipe(
     mergeMap(() =>
-      throwError(() => new Error('Something bad happened')).pipe(
-        catchError(err => of('The catchError operator'))
+      throwError(() => new Error("Something bad happened")).pipe(
+        catchError(() => of("Catching Error"))
       )
     ),
-    tap({next: console.log, error: console.warn})
+    tap({ next: console.log, error: console.warn })
   )
   .subscribe({
-    next: item => (result.textContent = item),
-    error: error => (result.textContent = `Error: ${error.message}`),
-    complete: () => (result.textContent = 'Complete')
+    next: (item) => (result.textContent = item),
+    error: (error) => (result.textContent = `Error: ${error.message}`),
+    complete: () => {
+      console.log("Complete");
+      result.textContent = "Complete";
+    },
   });
 
-fromEvent(btnClear, 'click').subscribe(() => (result.textContent = ''));
+fromEvent(btnClear, "click").subscribe(() => (result.textContent = ""));
 
-/*
+// .pipe(
+//   catchError((err) => of("The catchError operator"))
+// )
 
+// const apiCall$ = throwError(() => new Error("API request failed"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-if (module.hot) {
-  module.hot.dispose(function() {
-    location.reload();
-  });
-}
+// apiCall$
+//   .pipe(
+//     map((data) => {
+//       // This map function won't be executed because the observable errors
+//       return data;
+//     }),
+//     catchError((error) => {
+//       // Handle the error and return a fallback value
+//       console.error("Caught error:", error);
+//       return of("Fallback value");
+//     })
+//   )
+//   .subscribe({
+//     next: (value) => console.log("Received value:", value),
+//     error: (err) => console.log("Error:", err),
+//     complete: () => console.log("Complete"),
+//   });
