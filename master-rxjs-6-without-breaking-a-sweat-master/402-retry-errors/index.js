b78f8b2eaa9c07ec8629e1240b3d90a59c9cc705
rachit-anil <rachit9910102312@gmail.com>
2024-06-09 14:56:15 +0530

    Retry and Retry when

--- master-rxjs-6-without-breaking-a-sweat-master/402-retry-errors/index.js
+++ master-rxjs-6-without-breaking-a-sweat-master/402-retry-errors/index.js
@@ -1,57 +1,90 @@
-import { fromEvent, interval, switchMap, take, retry, retryWhen } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
+import {
+  fromEvent,
+  interval,
+  switchMap,
+  take,
+  retry,
+  retryWhen,
+  delay,
+  catchError,
+  mergeMap,
+  timer,
+  throwError,
+  of,
+} from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-const btnStart = document.getElementById('btnStart');
-const result = document.getElementById('result');
+const btnStart = document.getElementById("btnStart");
+const result = document.getElementById("result");
 
-fromEvent(btnStart, 'click')
+// fromEvent(btnStart, "click")
+//   .pipe(
+//     switchMap(() =>
+//       ajax.getJSON("http://localhost:8080/getJoke").pipe(
+//         // retry(3)
+
+//         retryWhen((err) => interval(2000).pipe(take(3)))
+//       )
+//     )
+//   )
+//   .subscribe({
+//     next: (item) => (result.textContent = item.value.joke),
+//     error: (error) => (result.textContent = `Error: ${error.message}`),
+//     complete: () => (result.textContent = "Complete"),
+//   });
+
+// ajax
+//   .getJSON("http://localhost:8080/getJokes")
+//   .pipe(
+//     retryWhen((errors) =>
+//       errors.pipe(
+//         switchMap((error, i) => {
+//           console.log(i);
+//           // if (i >= 5) {
+//           //   return throwError(() => new Error("Retries exceeded"));
+//           // }
+//           const backoffTime = Math.pow(2, i) * 1000; // Exponential backoff
+//           console.error(`Retrying in ${backoffTime / 1000} seconds...`);
+//           return timer(backoffTime);
+//         })
+//       )
+//     ),
+//     catchError((error) => {
+//       console.error("Final error:", error);
+//       return of("Fallback value");
+//     })
+//   )
+//   .subscribe({
+//     next: (data) => (result.textContent = data.value.joke),
+//     error: (error) => console.error("Error:", error),
+//     complete: () => console.log("Complete"),
+//   });
+
+// ajax.getJSON('http://localhost:8080/getJoke').
+
+// Exponential backoff
+fromEvent(btnStart, "click")
   .pipe(
-    switchMap(() =>
-      ajax.getJSON('http://theproblemsolver.nl/api/not-there').pipe(
-        // retry(3)
-
-        retryWhen(err => interval(1000).pipe(take(3)))
-      )
+    switchMap(() => {
+      return ajax.getJSON("http://localhost:8080/getJoke").pipe(
+        retryWhen((error) => {
+          return error.pipe(
+            switchMap((error, i) => {
+              if (i > 1) return throwError({ error: "Something went wrong" });
+              const backoffTime = Math.pow(2, i) * 1000;
+              console.error(`Retry in ${backoffTime / 1000} seconds`);
+              return timer(backoffTime);
+            })
+          );
+        })
+      );
+    }),
+    catchError((error) =>
+      of({ value: { joke: "API failed. Try to laugh on this joke !" } })
     )
   )
   .subscribe({
-    next: item => (result.textContent = item),
-    error: error => (result.textContent = `Error: ${error.message}`),
-    complete: () => (result.textContent = 'Complete')
+    next: (data) => (result.textContent = data.value.joke),
+    error: (error) => console.error("Error:", error),
+    complete: () => console.log("Complete"),
   });
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-if (module.hot) {
-  module.hot.dispose(function() {
-    location.reload();
-  });
-}
