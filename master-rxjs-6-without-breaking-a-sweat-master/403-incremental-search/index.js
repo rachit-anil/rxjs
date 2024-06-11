e81637b494e81f3fb9f6843269285e42672afbce
rachit-anil <rachit9910102312@gmail.com>
2024-06-11 22:26:48 +0530

    polling and incremental search

--- master-rxjs-6-without-breaking-a-sweat-master/403-incremental-search/index.js
+++ master-rxjs-6-without-breaking-a-sweat-master/403-incremental-search/index.js
@@ -15,35 +15,6 @@ import {
 } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
-// fromEvent(searchInput, "input")
-//   .pipe(
-//     map((e) => e.target.value),
-//     debounceTime(1000),
-//     distinctUntilChanged(),
-//     tap(() => (result.innerHTML = "")),
-//     filter((user) => !!user),
-//     switchMap((user) =>
-//       ajax
-//         .getJSON(`https://api.github.com/search/users?q=${user}`)
-//         .pipe(catchError((err) => EMPTY))
-//     ),
-//     map((rsp) => rsp.items),
-//     mergeMap((users) => users),
-//     tap({ next: (e) => console.log(e), error: (e) => console.warn(e) })
-//   )
-//   .subscribe((user) => {
-//     const div = document.createElement("div");
-//     div.innerHTML = `
-//       <div class="card" style="width: 18rem;">
-//         <img class="card-img-top" src="${user.avatar_url}" alt="Card image cap" crossOrigin="anonymous">
-//         <div class="card-body">
-//           <h5 class="card-title">${user.login}</h5>
-//           <a href="${user.html_url}" class="btn btn-primary">GitHub profile</a>
-//         </div>
-//       </div>`;
-//     result.appendChild(div);
-//   });
-
 const searchInput = document.getElementById("search");
 const result = document.getElementById("result");
 
@@ -52,19 +23,79 @@ fromEvent(searchInput, "input")
     map((e) => e.target.value),
     debounceTime(1000),
     distinctUntilChanged(),
+    tap(() => (result.innerHTML = "")),
     filter((user) => !!user),
     switchMap((user) =>
       ajax
         .getJSON(`https://api.github.com/search/users?q=${user}`)
-        .pipe(map((data) => data.items.map((item) => item.avatar_url)))
+        .pipe(catchError((err) => EMPTY))
     ),
-    switchMap((data) => data)
+    tap((x) => console.log(x)),
+    map((rsp) => rsp.items),
+    mergeMap((users) => users)
   )
-  .subscribe((avatarUrl) => {
+  .subscribe((user) => {
     const div = document.createElement("div");
     div.innerHTML = `
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="${avatarUrl}" alt="Card image cap" crossOrigin="anonymous">
-    </div>`;
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${user.avatar_url}" alt="Card image cap" crossOrigin="anonymous">
+        <div class="card-body">
+          <h5 class="card-title">${user.login}</h5>
+          <a href="${user.html_url}" class="btn btn-primary">GitHub profile</a>
+        </div>
+      </div>`;
     result.appendChild(div);
   });
+
+of([1, 2, 3, 4, 5])
+  .pipe(mergeMap((x) => x))
+  .subscribe((x) => console.log(x));
+
+//
+//
+
+//
+//
+
+//
+//
+
+//
+//
+
+//
+//
+
+//
+//
+
+//
+//
+
+//
+//
+// My implementation
+// const searchInput = document.getElementById("search");
+// const result = document.getElementById("result");
+
+// fromEvent(searchInput, "input")
+//   .pipe(
+//     map((e) => e.target.value),
+//     debounceTime(1000),
+//     distinctUntilChanged(),
+//     filter((user) => !!user),
+//     switchMap((user) =>
+//       ajax
+//         .getJSON(`https://api.github.com/search/users?q=${user}`)
+//         .pipe(map((data) => data.items.map((item) => item.avatar_url)))
+//     ),
+//     switchMap((data) => data)
+//   )
+//   .subscribe((avatarUrl) => {
+//     const div = document.createElement("div");
+//     div.innerHTML = `
+//     <div class="card" style="width: 18rem;">
+//       <img class="card-img-top" src="${avatarUrl}" alt="Card image cap" crossOrigin="anonymous">
+//     </div>`;
+//     result.appendChild(div);
+//   });
--- master-rxjs-6-without-breaking-a-sweat-master/404-ajax-polling/index.js
+++ master-rxjs-6-without-breaking-a-sweat-master/404-ajax-polling/index.js
@@ -1,65 +1,19 @@
-import { EMPTY, timer, map, catchError, mergeMap, switchMap, tap  } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
+import { EMPTY, timer, map, catchError, mergeMap, switchMap, tap } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-const url =
-  'http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript';
+const url = "http://localhost:8080/getJokes";
 
-const result = document.getElementById('result');
+const result = document.getElementById("result");
 
-const joke$ = timer(0, 5000).pipe(
-  tap(() => (result.innerHTML = '')),
-  switchMap(() =>
-    // Old: RxJS 6
-    // Fails because of an extra x-requested-with header
-    // is not allowed with the CORS request
-    // ajax.getJSON(url).pipe(catchError(() => EMPTY))
-
-    // New code. Works with RxJS 6 and 7.
-    ajax({url, crossDomain: true}).pipe(map(e => e.response), catchError(() => EMPTY))
-  ),
-  mergeMap((rsp) => rsp.value)
+const joke$ = timer(0, 1000).pipe(
+  mergeMap(() => ajax.getJSON(url).pipe(catchError(() => EMPTY)))
 );
 
-ajax({ url }).pipe(map((x) => x.response));
+// ajax({ url }).pipe(map((x) => x.response));
 
 joke$.subscribe((joke) => {
-  const li = document.createElement('li');
-  li.textContent = joke.joke;
+  console.log(joke);
+  const li = document.createElement("li");
+  li.textContent = joke.value.joke;
   result.appendChild(li);
 });
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-if (module.hot) {
-  module.hot.dispose(function () {
-    location.reload();
-  });
-}
